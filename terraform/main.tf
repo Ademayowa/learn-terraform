5695661c9a510c6ffe2f42e9cc77504d458dfdff
Ademayowa <ademayowaadeniyi@gmail.com>
2025-11-06 16:24:59 +0100

    chore: simplified terraform files

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,6 @@
+# DynamoDB Table
 resource "aws_dynamodb_table" "properties" {
-  name         = var.dynamodb_table_name
+  name         = "properties"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "id"
 
@@ -7,50 +8,38 @@ resource "aws_dynamodb_table" "properties" {
     name = "id"
     type = "S"
   }
-
-  tags = {
-    Name        = "${var.project_name}-properties-table"
-    Environment = var.environment
-  }
 }
 
+# Lambda Role
 resource "aws_iam_role" "lambda_role" {
   name = "${var.project_name}-lambda-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
       }
-    ]
+    }]
   })
 }
 
-resource "aws_iam_role_policy" "lambda_dynamodb" {
-  name = "${var.project_name}-lambda-dynamodb-policy"
+resource "aws_iam_role_policy" "lambda_policy" {
   role = aws_iam_role.lambda_role.id
 
   policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "dynamodb:PutItem",
-          "dynamodb:GetItem",
-          "dynamodb:UpdateItem",
-          "dynamodb:DeleteItem",
-          "dynamodb:Query",
-          "dynamodb:Scan"
-        ]
-        Resource = aws_dynamodb_table.properties.arn
-      }
-    ]
+    Statement = [{
+      Effect = "Allow"
+      Action = [
+        "dynamodb:PutItem",
+        "dynamodb:GetItem",
+        "dynamodb:Scan"
+      ]
+      Resource = aws_dynamodb_table.properties.arn
+    }]
   })
 }
 
@@ -59,14 +48,13 @@ resource "aws_iam_role_policy_attachment" "lambda_basic" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
+# Lambda Function
 resource "aws_lambda_function" "api" {
   filename         = "${path.module}/../build/lambda.zip"
   function_name    = "${var.project_name}-api"
   role             = aws_iam_role.lambda_role.arn
   handler          = "bootstrap"
   runtime          = "provided.al2023"
-  timeout          = 30
-  memory_size      = 256
   source_code_hash = filebase64sha256("${path.module}/../build/lambda.zip")
 
   environment {
@@ -74,21 +62,12 @@ resource "aws_lambda_function" "api" {
       DYNAMODB_TABLE = aws_dynamodb_table.properties.name
     }
   }
-
-  tags = {
-    Name        = "${var.project_name}-lambda"
-    Environment = var.environment
-  }
 }
 
+# API Gateway
 resource "aws_apigatewayv2_api" "api" {
   name          = "${var.project_name}-api"
   protocol_type = "HTTP"
-
-  tags = {
-    Name        = "${var.project_name}-api"
-    Environment = var.environment
-  }
 }
 
 resource "aws_apigatewayv2_integration" "lambda" {
@@ -98,7 +77,7 @@ resource "aws_apigatewayv2_integration" "lambda" {
   payload_format_version = "2.0"
 }
 
-resource "aws_apigatewayv2_route" "default" {
+resource "aws_apigatewayv2_route" "route" {
   api_id    = aws_apigatewayv2_api.api.id
   route_key = "ANY /{proxy+}"
   target    = "integrations/${aws_apigatewayv2_integration.lambda.id}"
@@ -110,31 +89,15 @@ resource "aws_apigatewayv2_route" "root" {
   target    = "integrations/${aws_apigatewayv2_integration.lambda.id}"
 }
 
-resource "aws_apigatewayv2_stage" "default" {
+resource "aws_apigatewayv2_stage" "stage" {
   api_id      = aws_apigatewayv2_api.api.id
   name        = "$default"
   auto_deploy = true
-
-  tags = {
-    Name        = "${var.project_name}-stage"
-    Environment = var.environment
-  }
 }
 
 resource "aws_lambda_permission" "api_gateway" {
-  statement_id  = "AllowAPIGatewayInvoke"
-  action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.api.function_name
+  action        = "lambda:InvokeFunction"
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${aws_apigatewayv2_api.api.execution_arn}/*/*"
-}
-
-output "api_endpoint" {
-  value       = aws_apigatewayv2_api.api.api_endpoint
-  description = "API Gateway endpoint URL"
-}
-
-output "dynamodb_table_name" {
-  value       = aws_dynamodb_table.properties.name
-  description = "DynamoDB table name"
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,29 +1,11 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+
 variable "project_name" {
-  description = "Name of the project"
-  type        = string
-  default     = "learn-terraform"
+  default = "learn-terraform"
 }
 
 variable "environment" {
-  description = "Environment name"
-  type        = string
-  default     = "dev"
-}
-
-variable "dynamodb_table_name" {
-  description = "DynamoDB table name for properties"
-  type        = string
-  default     = "properties"
-}
-
-variable "lambda_zip_path" {
-  description = "Path to the Lambda function zip file"
-  type        = string
-  default     = "../lambda.zip"
-}
-
-variable "aws_region" {
-  description = "AWS region"
-  type        = string
-  default     = "us-east-1"
+  default = "dev"
 }
